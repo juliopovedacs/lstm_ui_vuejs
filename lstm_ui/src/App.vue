9374a4d2336ed9c8cf8dde7437d65ed69a2bcc6d
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-11-19 22:20:35 -0500

    Added axios post request

--- lstm_ui/src/App.vue
+++ lstm_ui/src/App.vue
@@ -29,11 +29,17 @@ export default {
       this.todos = this.todos.filter(todo => todo.id !== id);
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
     }
   },
   created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos')
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
       .then(res => this.todos = res.data)
   }
 };
