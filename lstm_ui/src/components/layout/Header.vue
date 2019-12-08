ae0cd97d01972dbe34210f846b6397919b1a5f54
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-12-08 17:58:39 -0500

    Header style changed

--- lstm_ui/src/components/layout/Header.vue
+++ lstm_ui/src/components/layout/Header.vue
@@ -17,15 +17,23 @@ export default {
 
 <style scoped>
     .header {
-        background: #333;
+        background: rgb(30, 126, 216);
         color: #fff;
         text-align: center;
-        padding: 10px;
+        padding: 20px;
     }
 
     .header a {
         color: #fff;
         padding-right: 5px;
         text-decoration: none;
+    }
+    
+    #nav a.router-link-exact-active {
+        color: rgb(219, 13, 13)
+    }
+
+    #nav a {
+        margin-right: 10px;
     }
 </style>
