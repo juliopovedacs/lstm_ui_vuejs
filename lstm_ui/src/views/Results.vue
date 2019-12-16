7562fd0be7615e700270ebd291f2d192377ccb86
japoveda10 <ja.poveda10@uniandes.edu.co>
2019-12-16 17:19:04 -0500

    Results view reorganized

--- lstm_ui/src/views/Results.vue
+++ lstm_ui/src/views/Results.vue
@@ -10,17 +10,31 @@
 <template>
   <div class="results">
     <h1 id="resultsTitle">Results</h1>
-    <h4>Selected log:</h4>
-    <p id="selectedLogName">{{ this.$route.query.eventLogName }}</p>
-    <p id="eventLogIdParagraph" hidden>{{ this.$route.query.eventLogId }}</p>
-    <div class="text-center">
-      <div id="statistics">
-        <h4 id="accuracyTitle"><b>Accuracy:</b></h4>
-        <p id="accuracyValue">{{ this.selectedEventLogAccuracy }}</p>
-        <h4 id="similarityTitle" class="tooltip">Similarity:
-          <span class="tooltiptext">It is equal to (1-d), where d is the calculated Damerau-Levenshtein distance</span>
-        </h4>
-        <p id="similarityValue">{{ this.selectedEventLogSimilarity }}</p>
+    <div>
+      <div id="statistics" class="container">
+        <div class="column">
+          <h4>Selected log:</h4>
+          <p id="selectedLogName">{{ this.$route.query.eventLogName }}</p>
+          <p id="eventLogIdParagraph" hidden>{{ this.$route.query.eventLogId }}</p>
+        </div>
+        <div class="column">
+          <h4 id="accuracyTitle" class="tooltip">
+            Accuracy:
+            <span
+              class="tooltiptext"
+            >It is a value between 0 and 1. It is better if it is closer to 1</span>
+          </h4>
+          <p id="accuracyValue">{{ this.selectedEventLogAccuracy }}</p>
+        </div>
+        <div class="colum">
+          <h4 id="similarityTitle" class="tooltip">
+            Similarity:
+            <span
+              class="tooltiptext"
+            >It is equal to (1-d), where d is the calculated Damerau-Levenshtein distance</span>
+          </h4>
+          <p id="similarityValue">{{ this.selectedEventLogSimilarity }}</p>
+        </div>
       </div>
       <canvas id="results-chart"></canvas>
     </div>
@@ -41,7 +55,7 @@ export default {
     selectedEventLogName: String,
     selectedEventLogResults: Array,
     selectedEventLogAccuracy: Number,
-    selectedEventLogSimilarity: Number,
+    selectedEventLogSimilarity: Number
   },
   methods: {
     createChart(chartId, chartData) {
@@ -56,39 +70,59 @@ export default {
   },
   data() {
     return {
-      resultsChartData: resultsChartData,
-    }
+      resultsChartData: resultsChartData
+    };
   },
   created() {
     document.getElementById("myDiagramDiv").style.display = "none";
   },
   mounted() {
-    this.createChart('results-chart', this.resultsChartData);
+    this.createChart("results-chart", this.resultsChartData);
     var eventLogId = document.getElementById("eventLogIdParagraph").innerHTML;
 
     axios
       .get(`http://127.0.0.1:8000/event_logs/${eventLogId}/trained_model/`)
       .then(res => (this.selectedEventLogAccuracy = res.data))
       .catch(err => console.log(err));
-    
+
     this.selectedEventLogAccuracy = 0.75;
-    
+
     axios
       .get(`http://127.0.0.1:8000/event_logs/${eventLogId}/results/`)
       .then(res => (this.selectedEventLogResults = res.data))
       .catch(err => console.log(err));
-    
+
     this.selectedEventLogSimilarity = 0.74;
   }
 };
 
-$(document).ready(function(){
-  $('[data-toggle="popover"]').popover();   
+$(document).ready(function() {
+  $('[data-toggle="popover"]').popover();
 });
 </script>
 
 <!-- CSS -->
 <style scoped>
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 10px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+}
+
 .tooltip {
   position: relative;
   display: inline-block;
@@ -96,7 +130,7 @@ $(document).ready(function(){
 
 .tooltip .tooltiptext {
   visibility: hidden;
-  width: 600px;
+  width: 120px;
   background-color: rgb(30, 126, 216);
   color: #fff;
   text-align: center;
@@ -104,19 +138,20 @@ $(document).ready(function(){
   padding: 5px 0;
   position: absolute;
   z-index: 1;
-  top: -5px;
-  left: 110%;
+  bottom: 150%;
+  left: 50%;
+  margin-left: -60px;
 }
 
 .tooltip .tooltiptext::after {
-  content: "";
+  content: " ";
   position: absolute;
-  top: 50%;
-  right: 100%;
-  margin-top: -5px;
+  top: 100%; /* At the bottom of the tooltip */
+  left: 50%;
+  margin-left: -5px;
   border-width: 5px;
   border-style: solid;
-  border-color: transparent rgb(30, 126, 216) transparent transparent;
+  border-color: rgb(30, 126, 216) transparent transparent transparent;
 }
 .tooltip:hover .tooltiptext {
   visibility: visible;
@@ -131,15 +166,11 @@ $(document).ready(function(){
 }
 
 #selectedLogName {
-  margin-bottom: 5px;
-}
-
-#accuracyTitle {
-  margin-bottom: 5px;
+  margin-bottom: 10px;
 }
 
 #accuracyValue {
-  margin-bottom: 5px;
+  margin-bottom: 10px;
 }
 
 #similarityTitle {
