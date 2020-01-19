75c2ec0020b6973c809ca0db7f79c89595c74557
japoveda10 <ja.poveda10@uniandes.edu.co>
2020-01-19 18:19:22 -0500

    Added Running Case Item component

--- lstm_ui/src/components/RunningCases.vue
+++ lstm_ui/src/components/RunningCases.vue
@@ -3,14 +3,19 @@
     <h2>Running Cases</h2>
     <br>
     <div v-bind:key="runningCase.id" v-for="runningCase in runningCases">
-        <h3>{{ runningCase.name }}</h3>
+        <RunningCaseItem v-bind:runningCase="runningCase" />
     </div>
 </div>  
 </template>
 
 <script>
+import RunningCaseItem from './RunningCaseItem.vue';
+
 export default {
     name: "RunningCases",
+    components: {
+        RunningCaseItem
+    },
     props: ["runningCases"]
 }
 </script>
